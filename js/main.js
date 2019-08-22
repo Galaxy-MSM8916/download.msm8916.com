45af0f842a06121d8534417b614286728d5dc0fe
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-08-22 11:44:47 -0400

    js: main: Add function to get url params

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+function get_param($key)
+{
+    $value = null;
+
+    $arr = document.URL.split("?");
+
+    if ($arr.length < 2)
+        return $value;
+
+    $query = $arr[1];
+
+    $params = new URLSearchParams($query);
+
+    $value = $params.get($key);
+
+    return $value;
+}
+
 function set_title()
 {
     var $title = document.getElementsByTagName("title")[0].innerHTML;
