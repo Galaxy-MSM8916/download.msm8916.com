7d21f95c6f2094c5aefbafb6b497b42ae111686a
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-09-04 00:51:43 -0400

    config: Add include for sensitive config options

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php namespace download;
 
+    include "config.secure.php";
+
     $cfg['releases']['request_url'] = "https://api.github.com/repos/Galaxy-MSM8916/releases/releases?page=";
     $cfg['releases']["cache_dir"] = "releases_zlib";
     $cfg['releases']["compress"] = true;
